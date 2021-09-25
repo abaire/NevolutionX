0557831156e292c1153c0292d07f1a674d094692
Erik Abair <erik.abair@gmail.com>
2021-09-24 22:48:18 -0700

    Hackery attempting to save framebuffer.

--- Includes/xbeLauncher.cpp
+++ Includes/xbeLauncher.cpp
@@ -13,11 +13,76 @@ void XBELauncher::launch(std::string const& xbePath) {
   XLaunchXBE(const_cast<char*>(xbePath.c_str()));
 }
 
+
+typedef struct _X_D3DSURFACE_DESC {
+  DWORD Format;
+  DWORD Type;
+  DWORD Usage;
+  UINT Size;
+  DWORD MultiSampleType;
+  UINT Width;
+  UINT Height;
+} surface_desc;
+
+struct X_D3DResource {
+  DWORD Common{ 0x00050000 }; // X_D3DCOMMON_TYPE_SURFACE
+  DWORD Data{ 0 };
+  DWORD Lock{ 0 };
+};
+
+struct X_D3DPixelContainer : public X_D3DResource {
+  DWORD Format{ 1 }; // X_D3DFORMAT_DMACHANNEL_A
+  DWORD Size{ 0 }; // Size of a non power-of-2 texture, must be zero otherwise
+};
+
+struct X_D3DSurface : public X_D3DPixelContainer {
+  /*X_D3DBaseTexture*/ void* Parent{ nullptr };
+};
+
 void XBELauncher::showLaunchImage() {
   VIDEO_MODE mode = XVideoGetMode();
 
-  // TODO(XboxDev/nxdk#507): Display launch image instead when framebuffer can be persisted.
+  int bytesPerPixel = (mode.bpp + 7) / 8;
+  int pitch = mode.width * bytesPerPixel;
+  int screenSize = pitch * mode.height;
+
+  surface_desc s;
+  s.Format = 0x06; // X_D3DFMT_A8R8G8B8
+  s.Type = 1; // X_D3DRTYPE_SURFACE
+  s.Usage = 0;
+  s.Size = 0; // TODO: Is this size, or is this memory pool?
+  s.MultiSampleType = 0x0011; // NONE
+  s.Width = mode.width;
+  s.Height = mode.height;
+
+  X_D3DSurface surface;
+  // The Size field of a container will be zero if the texture is swizzled or compressed.
+// It is guarenteed to be non-zero otherwise because either the height/width will be
+// greater than one or the pitch adjust will be nonzero because the minimum texture
+// pitch is 8 bytes.
+#define X_D3DSIZE_WIDTH_MASK 0x00000FFF // Width of the texture - 1, in texels
+//#define X_D3DSIZE_WIDTH_SHIFT           0
+#define X_D3DSIZE_HEIGHT_MASK 0x00FFF000 // Height of the texture - 1, in texels
+#define X_D3DSIZE_HEIGHT_SHIFT 12
+#define X_D3DSIZE_PITCH_MASK 0xFF000000 // Pitch / 64 - 1
+#define X_D3DSIZE_PITCH_SHIFT 24
+  surface.Size = (mode.width & X_D3DSIZE_WIDTH_MASK)
+                 | ((mode.height << X_D3DSIZE_HEIGHT_SHIFT) & X_D3DSIZE_HEIGHT_MASK)
+                 | (((pitch / 64 - 1) << X_D3DSIZE_PITCH_SHIFT) & X_D3DSIZE_PITCH_MASK);
+
+  DWORD totalSize = sizeof(surface) + screenSize;
+
+  unsigned char* framebufferMemory = (unsigned char*)MmAllocateContiguousMemoryEx(
+      totalSize, 0x00000000, 0x7FFFFFFF, 0x1000, PAGE_READWRITE | PAGE_WRITECOMBINE);
+
+  memcpy(framebufferMemory, &surface, sizeof(surface));
   unsigned char* fb = XVideoGetFB();
+
+  memcpy(framebufferMemory + sizeof(surface), fb, screenSize);
+  MmPersistContiguousMemory(framebufferMemory, totalSize, TRUE);
+  AvSetSavedDataAddress(framebufferMemory);
+
+  // TODO(XboxDev/nxdk#507): Display launch image instead when framebuffer can be persisted.
   memset(fb, 0, mode.width * mode.height * (mode.bpp >> 3));
   XVideoFlushFB();
 }
